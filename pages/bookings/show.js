63b2db366dedeb1a3a7f05ef0936f891288c058b
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-23 09:24:51 +0800

    added tab styling to tab bar

--- pages/bookings/show.js
+++ pages/bookings/show.js
@@ -22,12 +22,6 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-    if (typeof this.getTabBar === 'function' &&
-        this.getTabBar()) {
-        this.getTabBar().setData({
-          selected: 1
-        })
-      }
   },
 
   /**
