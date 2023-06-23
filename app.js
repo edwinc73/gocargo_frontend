cab3f14b632c535e3c0e212cd64640e6d2f44260
fanxin li <lexxshinjj@gmail.com>
2023-06-23 16:15:37 +0800

    authentication

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    const app = this
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        wx.request({
+          url:  `${app.globalData.baseUrl}/login`,
+          method: 'post',
+          data: { code: res.code }, 
+          success(loginRes) {
+            app.globalData.user = loginRes.data.user 
+            app.globalData.header = loginRes.data.headers
+            console.log(loginRes)
+          }
+        })
       }
     })
     wx.loadFontFace({
@@ -18,6 +28,10 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    header: null,
+    user: null,
+    userInfo: null,
+    baseUrl: `http://127.0.0.1:3000/api/v1`
+    // https://gocargo-rails.osc-fr1.scalingo.io
   }
 })
